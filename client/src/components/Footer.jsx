9add4a722e4b5ac4a4307a284bab7e509e889c9c
= <marcjosephcruz24@gmail.com>
2025-09-01 22:35:42 +0800

    feat: add footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <div className="bg-gray-200 text-gray-90 py-6">
+      <p className="text-sm text-center text-gray-600">
+        &copy; {new Date().getFullYear()} Team CampusBallot. All rights
+        preserved
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
