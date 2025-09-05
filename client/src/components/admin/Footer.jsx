dab57d1a08c62c7125ec5d5d9b4c29fe75e33f4e
= <marcjosephcruz24@gmail.com>
2025-09-05 16:23:06 +0800

    style: update footer text size for improved readability

--- client/src/components/admin/Footer.jsx
+++ client/src/components/admin/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <div className="bg-gray-200 text-gray-90 py-6">
-      <p className="text-sm text-center text-gray-600">
+      <p className="text-xs md:text-sm text-center text-gray-600">
         &copy; {new Date().getFullYear()} Team CampusBallot. All rights
         preserved
       </p>
